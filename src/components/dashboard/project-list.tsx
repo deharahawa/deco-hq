27ac168899f84dfb416ff483f46bf6dd500828c7
deharahawa <aearahawa@gmail.com>
2026-02-16 16:52:56 -0300

    feat: adjust imports

--- src/components/dashboard/project-list.tsx
+++
@@ -1,9 +0,0 @@
-
-import { getActiveProjects } from "@/actions/projects";
-import ProjectListClient from "./project-list-client";
-
-export default async function ProjectList() {
-  const projects = await getActiveProjects();
-  
-  return <ProjectListClient projects={projects} />;
-}
